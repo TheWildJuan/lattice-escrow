924b4a7516ec689f4b4fdefecfe4248b7014caa9
TheWildJuan <siksnikes94@gmail.com>
2026-03-12 03:46:32 +0000

    Deploy to devnet — Program ID AJB7Jj5MCeGjv9NgeAeZzKBYaHR4jsL8XZW2Dxz8hZdf

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program;
 
-declare_id!("11111111111111111111111111111111"); // replaced on deploy
+declare_id!("AJB7Jj5MCeGjv9NgeAeZzKBYaHR4jsL8XZW2Dxz8hZdf"); // replaced on deploy
 
 #[program]
 pub mod escrow {
